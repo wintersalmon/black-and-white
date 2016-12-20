2df304e0267858518ce3372d08209a534db0d20d
wintersalmon <salmonjoon@gmail.com>
2016-12-20 15:18:42 +0900

    minor change to test script

--- main_test.py
+++ main_test.py
@@ -5,25 +5,19 @@ WinterSalmon
 Main Test
 '''
 
-from gui.board_drawer import BoardDrawer
+from cli.board_drawer import BoardDrawer
 from data.board.board import Board
 from data.board.tile import TileWW, TileWG, TileWB, TileGB
 from data.board.direction import DIRECTION
 
-
-def test_board():
+def create_board_type_one(max_row, max_col):
     '''
     Method Description
     '''
-    row_count = 5
-    col_count = 12
-    board = Board(row_count, col_count)
-    board_drawer = BoardDrawer(board)
+    board = Board(max_row, max_col)
 
-    board_drawer.draw_color()
-    board_drawer.draw_overlap_counter()
-    for row in range(row_count):
-        for col in range(col_count-1):
+    for row in range(max_row):
+        for col in range(max_col-1):
             tile = None
             if col%3 == 0:
                 tile = TileWW()
@@ -35,8 +29,20 @@ def test_board():
                 tile = TileGB()
 
             direction = DIRECTION.RIGHT
-            result = board.place_tile(tile, row, col, direction)
-            print(row, col, result)
+            board.place_tile(tile, row, col, direction)
+
+    return board
+
+
+def test_board():
+    '''
+    Method Description
+    '''
+    max_row = 5
+    max_col = 12
+    board = create_board_type_one(max_row, max_col)
+    board_drawer = BoardDrawer(board)
+
     board_drawer.draw_color()
     board_drawer.draw_overlap_counter()
 
