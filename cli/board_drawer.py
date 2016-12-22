9f49091c3fc9b121c278571bbd9d4a0949d218d6
wintersalmon <salmonjoon@gmail.com>
2016-12-22 15:55:53 +0900

    fix marker overlap color issue

--- cli/board_drawer.py
+++ cli/board_drawer.py
@@ -21,6 +21,7 @@ class BoardDrawer():
         self.row_count = board.get_row_count()
         self.col_count = board.get_col_count()
         self.marker = [[None for col in range(self.col_count)] for row in range(self.row_count)]
+        self.b_marker = [[None for col in range(self.col_count)] for row in range(self.row_count)]
 
 
     def draw_color(self):
@@ -33,13 +34,15 @@ class BoardDrawer():
                 for col in range(self.board.get_col_count()):
                     color = self.board.get_block_color(row, col)
                     if self.marker[row][col]:
-                        if isinstance(self.marker[row][col], Block):
+                        if self.b_marker[row][col]:
                             overlap_color = self.marker[row][col].get_color()
                             mixed_color = COLOR.mix_color(color, overlap_color)
                             color_text = color.get_color_text(mixed_color)
                             color_text = '[' + color_text + ']'
                         else:
-                            color_text = color.get_color_text(color)
+                            overlap_color = self.marker[row][col].get_color()
+                            mixed_color = COLOR.mix_color(color, overlap_color)
+                            color_text = color.get_color_text(mixed_color)
                             color_text = '<' + color_text + '>'
                     else:
                         color_text = color.get_color_text(color)
@@ -71,6 +74,7 @@ class BoardDrawer():
         for row in range(self.row_count):
             for col in range(self.col_count):
                 self.marker[row][col] = None
+                self.b_marker[row][col] = False
 
 
     def set_marker(self, tile, row, col, direction):
@@ -81,13 +85,19 @@ class BoardDrawer():
         if self.board.can_place_tile(tile, row, col, direction):
 
             self.marker[row][col] = tile.get_block(0)
+            self.b_marker[row][col] = True
 
             ad_row, ad_col = DIRECTION.adjust_row_col_by_direction(row, col, direction)
+
             self.marker[ad_row][ad_col] = tile.get_block(1)
+            self.b_marker[ad_row][ad_col] = True
         else:
             if row >= 0 and row < self.row_count and col >= 0 and col < self.col_count:
-                self.marker[row][col] = True
+                self.marker[row][col] = tile.get_block(0)
+                self.b_marker[row][col] = False
 
-            row, col = DIRECTION.adjust_row_col_by_direction(row, col, direction)
-            if row >= 0 and row < self.row_count and col >= 0 and col < self.col_count:
-                self.marker[row][col] = True
+            ad_row, ad_col = DIRECTION.adjust_row_col_by_direction(row, col, direction)
+
+            if ad_row >= 0 and ad_row < self.row_count and ad_col >= 0 and ad_col < self.col_count:
+                self.marker[ad_row][ad_col] = tile.get_block(1)
+                self.b_marker[ad_row][ad_col] = False
