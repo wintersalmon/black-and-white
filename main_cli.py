f2ca32654fc3f204b7b743c8f4a311e1b8854ba2
wintersalmon <salmonjoon@gmail.com>
2017-01-15 16:15:19 +0900

    minor changes

---
+++ main_cli.py
@@ -0,0 +1,13 @@
+'''
+BLACK-AND-WHITE
+WinterSalmon
+Main Cli
+'''
+
+from cli.cli import Cli
+
+if __name__ == "__main__":
+    CLI = Cli()
+    CLI.init_screen()
+    CLI.game_screen()
+    CLI.credit_screen()
